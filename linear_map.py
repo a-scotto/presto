5b5bd3ad3998a9e34d2a89a7b32949a960c47649
a-scotto <alex_scotto@live.fr>
2019-04-24 17:39:25 +0200

    Add Projector as LinearMap

--- linear_map.py
+++ linear_map.py
@@ -112,27 +112,6 @@ class LinearMap(object):
         return self + (-B)
 
 
-class LinearMapInverse(LinearMap):
-
-    def __init__(self, A):
-
-        if not isinstance(A, LinearMap):
-            raise LinearMapError('Requires a Matrix object for solving.')
-
-        if A.shape[0] != A.shape[1]:
-            raise LinearMapError('Impossible to solve non square Matrix.')
-
-        super().__init__(A.shape, A.dtype, self._dot, self._dot_adj)
-
-        self._A = A.canonical_repr()
-
-    def _dot(self, X):
-        return numpy.linalg.solve(self._A, X)
-
-    def _dot_adj(self, X):
-        return numpy.linalg.solve(self._A.T.conj(), X)
-
-
 class _ScaleLinearMap(LinearMap):
 
     def __init__(self, A, scalar):
@@ -215,27 +194,6 @@ class _AdjointLinearMap(LinearMap):
         super().__init__((p, n), A.dtype, A._dot_adj, A._dot)
 
 
-class Matrix(LinearMap):
-
-    def __init__(self, A):
-
-        if not type(A) == numpy.ndarray:
-            raise LinearMapError('Matrix objects must be defined from a numpy.ndarray')
-
-        super().__init__(A.shape, A.dtype, self._dot, self._dot_adj)
-
-        self._A = A
-
-    def _dot(self, X):
-        return self._A @ X
-
-    def _dot_adj(self, X):
-        return self._A.T.conj() @ X
-
-    def __repr__(self):
-        return self._A.__repr__()
-
-
 class IdentityMap(LinearMap):
 
     def __init__(self, shape):
@@ -304,65 +262,6 @@ class DiagonalMap(LinearMap):
         return numpy.diag(self.diag)
 
 
-class UpperTriangularMap(LinearMap):
-
-    def __init__(self, T):
-
-        if not type(T) == numpy.ndarray:
-            raise LinearMapError('UpperTriangular objects must be defined from a numpy.ndarray')
-
-        if T.shape[0] != T.shape[1]:
-            raise LinearMapError('Upper triangular operator must be of square shape.')
-
-        super().__init__(T.shape, T.dtype, self._dot, self._dot_adj)
-
-        self._T = numpy.triu(T)
-
-    def _dot(self, X):
-        n, _ = self.shape
-        ret = numpy.zeros_like(X)
-        for i in range(n):
-            ret[i, :] = self._T[[i], i:] @ X[i:, :]
-        return ret
-
-    def _dot_adj(self, X):
-        n, _ = self.shape
-        ret = numpy.zeros_like(X)
-        for i in range(n):
-            ret[i, :] = self._T[:(i + 1), i] @ X[:(i + 1), :]
-        return ret
-
-    def __repr__(self):
-        return self._A.__repr__()
-
-
-class SelfAdjointMap(LinearMap):
-
-    def __init__(self, A):
-
-        if not type(A) == numpy.ndarray:
-            raise LinearMapError('SelfAdjointMap objects must be defined from a numpy.ndarray')
-
-        if A.shape[0] != A.shape[1]:
-            raise LinearMapError('Self-adjoint operator must be of square shape.')
-
-        T = numpy.triu(A)
-        T[numpy.diag_indices_from(T)] /= 2
-
-        super().__init__(T.shape, T.dtype, self._dot, self._dot_adj)
-
-        self._half = UpperTriangularMap(T)
-
-    def _dot(self, X):
-        return self._half.dot(X) + self._half.dot_adj(X)
-
-    def _dot_adj(self, X):
-        return self._half.dot(X) + self._half.dot_adj(X)
-
-    def __repr__(self):
-        return self._A.__repr__()
-
-
 class Projector(LinearMap):
     """
     Abstract class for projectors. In the initialization of the Projector, X denotes the range
@@ -391,60 +290,80 @@ class Projector(LinearMap):
         # If orthogonal projector
         if Y is X:
             if orthogonalize:
-                self.V, self.R_v = scipy.linalg.qr(X)
+                self.V, self.R_v = qr(X)
+                self.Q, self.R = None, None
             else:
                 self.V, self.R_v = X, None
+                self.Q, self.R = scipy.linalg.qr(self.V.T.dot(self.V))
 
             self.W, self.R_w = self.V, self.R_v
-
-            self.Q, self.R = None, None
 
         # If oblique projector
         else:
             if orthogonalize:
-                self.V, self.R_v = scipy.linalg.qr(X)
-                self.W, self.R_w = scipy.linalg.qr(Y)
+                self.V, self.R_v = qr(X)
+                self.W, self.R_w = qr(Y)
             else:
                 self.V, self.R_v = X, None
                 self.W, self.R_w = Y, None
 
-            self.W, self.R_w = self.V, self.R_v
-
+            print(self.W.T.dot(self.V))
             self.Q, self.R = scipy.linalg.qr(self.W.T.dot(self.V))
 
     def _dot(self, X):
-        # If orthogonal projector
+        # If orthogonalized
         if self.Q is None:
             return self.V.dot(self.W.T.dot(X))
-        # If oblique projector
         else:
-            return self.V.dot(scipy.linalg.solve_triangular(self.R, self.Q.T.dot(self.W.T.dot(X))))
+            c = self.Q.T.dot(self.W.T.dot(X))
+            return self.V.dot(scipy.linalg.solve_triangular(self.R, c))
 
     def _dot_adj(self, X):
-        # If orthogonal projector
+        # If orthogonalized
         if self.Q is None:
             return self.W.dot(self.V.T.dot(X))
-        # If oblique projector
         else:
-            return self.W.dot(self.Q(scipy.linalg.solve_triangular(self.R,
-                                                                   self.V.T.dot(X),
-                                                                   lower=True)))
+            c = self.V.T.dot(X)
+            return self.W.dot(self.Q.dot(scipy.linalg.solve(self.R.T, c, lower=True)))
+
+    def get_complement(self):
+        return IdentityMap(self.shape) - self
 
 
 if __name__ == "__main__":
 
-    S = numpy.random.rand(10, 3)
-    S, _ = scipy.linalg.qr(S)
+    import numpy
+    from numpy.linalg import norm
 
-    S_orth = numpy.random.rand(10, 3)
-    S_orth -= S.dot(S.T.dot(S_orth))
+    size = 20
+    n = 4
+    orthogonalize = True
 
-    P = Projector(S)
+    V = numpy.random.rand(size, n)
+    W = numpy.random.rand(size, n)
+    S = numpy.random.rand(size, n)
 
-    print(numpy.linalg.norm(P.dot(S) - S))
-    print(numpy.linalg.norm(P.dot(S_orth)))
+    P_ortho = Projector(V, None, orthogonalize)
+    P_obliq = Projector(V, W, orthogonalize)
+    P_adjnt = Projector(W, V, orthogonalize)
 
-    P_s = P.adjoint()
+    print('Range validation :')
+    print(norm(P_ortho.dot(V) - V))
+    print(norm(P_obliq.dot(V) - V))
+    print()
 
-    print(numpy.linalg.norm(P_s.dot(S_orth) - S_orth))
-    print(numpy.linalg.norm(P_s.dot(S)))
+    V_orth = S - P_ortho.dot(S)
+    W_orth = S - P_obliq.dot(S)
+
+    print('Kernel validation :')
+    print(norm(P_ortho.dot(V_orth)))
+    print(norm(P_obliq.dot(W_orth)))
+    print()
+
+    print('Adjonction validation:')
+    print(norm(P_adjnt.dot(S) - P_obliq.dot_adj(S)))
+    print()
+
+    print('Complement Projector valdiation')
+    P_comp = P_obliq.get_complement()
+    print(norm(S - P_comp.dot(S) - P_obliq.dot(S)))
