fac7a367f95dd96086c3e003a1f8ff6d9f4b920f
a-scotto <alex_scotto@live.fr>
2019-09-24 09:04:57 +0200

    Modifify the plots and remove the comparison criterion choice.

--- postprocess.py
+++ postprocess.py
@@ -10,10 +10,12 @@ Description:
 """
 
 import os
-import numpy
 import argparse
 
-from matplotlib import pyplot
+from matplotlib import pyplot, rc
+from utils.postprocessing import read_report
+
+rc('text', usetex=True)
 
 COLORS = ['tab:blue',
           'tab:orange',
@@ -35,13 +37,8 @@ parser.add_argument('-r',
                     required=True,
                     dest='reports',
                     help='Paths of reports text files to post-process.')
-parser.add_argument('-c',
-                    required=True,
-                    dest='comparison',
-                    choices=['pb', 'spl'],
-                    help='Comparison criteria, either problems or sampling methods.')
+
 args = parser.parse_args()
-comparison = args.comparison
 
 reports_sets = dict()
 
@@ -49,123 +46,73 @@ reports_sets = dict()
 for REPORT_PATH in args.reports:
     _, report_name = os.path.split(REPORT_PATH)
 
+    # Skip not concerned files
+    if report_name.endswith('.npz') or report_name.endswith('.npy'):
+        continue
+
     problem_name = report_name.split('_')[0]
     sampling = report_name.split('_')[-1]
 
-    # Sort the reports according to the comparison criterion provided
-    if comparison == 'spl':
-        if problem_name not in reports_sets.keys():
-            reports_sets[problem_name] = [REPORT_PATH]
-        else:
-            reports_sets[problem_name].append(REPORT_PATH)
+    # Aggregate the reports path according to the problem tested
+    if problem_name not in reports_sets.keys():
+        reports_sets[problem_name] = [REPORT_PATH]
     else:
-        if sampling not in reports_sets.keys():
-            reports_sets[sampling] = [REPORT_PATH]
-        else:
-            reports_sets[sampling].append(REPORT_PATH)
+        reports_sets[problem_name].append(REPORT_PATH)
 
 # Go through all the reports
-for key, reports_paths in reports_sets.items():
-    pyplot.figure()
-    pyplot.grid()
-
-    # Set corresponding title regarding the comparison criterion
-    if comparison == 'spl':
-        plot_title = 'Sampling strategies results on ' + key + ' problem'
-    else:
-        plot_title = 'Operators preconditioned by ' + key + ' sampled subspaces.'
+for operator, reports_paths in reports_sets.items():
 
     # Browse through the reports file names in the report set
     for j, REPORT_PATH in enumerate(reports_paths):
-        if 'worse' in REPORT_PATH or 'best' in REPORT_PATH:
-            continue
+        pyplot.figure()
+        pyplot.grid()
 
-        _, report_name = os.path.split(REPORT_PATH)
+        metadata, deterministic_data, stochastic_data = read_report(REPORT_PATH)
 
-        if comparison == 'spl':
-            label = report_name.split('_')[-1]
-        else:
-            label = report_name.split('_')[0]
+        pyplot.hlines(y=metadata['reference'],
+                      xmin=stochastic_data['sizes'][0] / metadata['size'],
+                      xmax=stochastic_data['sizes'][-1] / metadata['size'],
+                      linestyle='dashed',
+                      label='CG with only first-level preconditioner.')
 
-        det_perfs = list()
-        sto_sizes = list()
-        det_sizes = list()
-        means = list()
-        stdevs = list()
-        minima = list()
-        maxima = list()
+        pyplot.errorbar(stochastic_data['sizes'] / metadata['size'],
+                        stochastic_data['means'] * metadata['reference'],
+                        yerr=stochastic_data['stdevs'] * metadata['reference'],
+                        fmt='o-',
+                        mec='k',
+                        ms=4,
+                        elinewidth=0.5,
+                        ecolor='k',
+                        capsize=2.5,
+                        label='Random sparse subspace: $\\mu \\pm \\sigma$.')
 
-        # Read and store the data contained in reports files
-        with open(REPORT_PATH, 'r') as report:
-            # Get the list of text lines
-            content = report.readlines()
-
-            for data in content:
-                # Skip report header
-                if data[0] == '>':
-                    pass
-
-                # Read operator metadata
-                elif '~operator_metadata' in data:
-                    metadata = data.split('#')[1:]
-                    size = int(metadata[0])
-                    nnz = int(metadata[1])
-                    cond = float(metadata[2])
-                    source = metadata[3]
-
-                # Read benchmark setup metadata
-                elif '~benchmark_metadata' in data:
-                    metadata = data.split('#')[1:]
-                    reference_run = int(metadata[0])
-                    d = float(metadata[1])
-
-                # Read data
-                else:
-                    det_data, sto_data = data.split('#')
-                    k_0, det_perf = det_data.split('_')
-                    k, sto_perf = sto_data.split('_')
-
-                    sto_perf = list(map(lambda x: float(x), sto_perf.split(',')))
-
-                    sto_sizes.append(int(k))
-                    det_sizes.append(int(k_0))
-
-                    det_perfs.append(float(det_perf))
-                    means.append(numpy.mean(sto_perf))
-                    stdevs.append(numpy.std(sto_perf))
-                    minima.append(min(sto_perf))
-                    maxima.append(max(sto_perf))
-
-        lower = [means[i] - stdevs[i] for i in range(len(means))]
-        upper = [means[i] + stdevs[i] for i in range(len(means))]
-
-        sto_sizes = numpy.asarray(sto_sizes)
-        det_sizes = numpy.asarray(det_sizes)
-
-        stochastic_cost = 4 * nnz + 6 * (sto_sizes * (1 - d) + size) + sto_sizes**2
-        deterministic_cost = 8 * size * det_sizes
-
-        # Add plot to the current figure
-        pyplot.plot(sto_sizes / size,
-                    means,
-                    linestyle='-',
+        pyplot.plot(stochastic_data['sizes'] / metadata['size'],
+                    deterministic_data['0'] * metadata['reference'],
+                    color=COLORS[j % len(COLORS) + 1],
+                    marker='^',
                     mec='k',
-                    ms=5,
-                    marker=MARKER[(j // len(COLORS)) % len(MARKER)],
-                    color=COLORS[j % len(COLORS)],
-                    label=label)
+                    ms=4,
+                    label='Informed descent directions.')
 
-        pyplot.fill_between(sto_sizes / size, lower, upper, color=COLORS[j % len(COLORS)], alpha=0.2)
-        pyplot.plot(sto_sizes / size, minima, color=COLORS[j % len(COLORS)], marker='+', lw=0)
-        pyplot.plot(sto_sizes / size, det_perfs, color=COLORS[j % len(COLORS)], ls='--')
+        pyplot.plot(stochastic_data['sizes'] / metadata['size'],
+                    deterministic_data['1'] * metadata['reference'],
+                    color=COLORS[j % len(COLORS) + 2],
+                    marker='<',
+                    mec='k',
+                    ms=4,
+                    label='Ritz vectors associated with highest Ritz values.')
 
-    # Add the legend, title, and axis titles
-    pyplot.legend()
-    pyplot.title(plot_title)
-    pyplot.xlabel('Ratios of subspace size and problem size k / n')
-    pyplot.ylabel('PCG iterations compared to ' + str(reference_run))
+        # Set corresponding title regarding the comparison criterion
+        plot_title = 'LMP results on $A=$ {}, $n=$ {}, $M=$ {}'\
+            .format(operator, metadata['size'], metadata['first_lvl_preconditioner'].capitalize())
 
-    # pyplot.xlim(subspace_sizes[0], subspace_sizes[-1])
-    pyplot.ylim(bottom=0.)
+        # Add the legend, title, and axis titles
+        pyplot.legend()
+        pyplot.title(plot_title)
+        pyplot.xlabel('Subspace size as a fraction of $n$.')
+        pyplot.ylabel('Preconditioned CG number of iterations.')
+        pyplot.xlim(stochastic_data['sizes'][0] / metadata['size'],
+                    stochastic_data['sizes'][-1] / metadata['size'])
+        pyplot.ylim(bottom=0.)
 
 pyplot.show()
