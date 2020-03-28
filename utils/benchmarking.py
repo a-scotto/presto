a5e712686548ad3d1637de56cdaad2151c0c6253
a-scotto <alex_scotto@live.fr>
2020-03-28 13:29:51 +0100

    Include modifications of TestOperator class and subspace factories.

--- utils/benchmarking.py
+++ utils/benchmarking.py
@@ -21,7 +21,7 @@ import scipy.optimize
 from utils.operator import TestOperator
 from core.linear_operator import SelfAdjointMatrix
 from core.linear_system import ConjugateGradient, LinearSystem
-from core.projection_subspace import RandomSubspaceFactory, KrylovSubspaceFactory
+from core.projection_subspace import RandomSubspaceFactory, DeterministicSubspaceFactory
 from core.preconditioner import LimitedMemoryPreconditioner, AlgebraicPreconditionerFactory
 
 
@@ -39,7 +39,7 @@ def process_precond_data(precond_parameters: str) -> dict:
     :param precond_parameters: Parameters of the preconditioner to benchmark.
     """
 
-    subspace, first_level_precond = precond_parameters.split(',')
+    subspace, first_precond = precond_parameters.split(',')
 
     # Retrieve subspace information
     try:
@@ -57,7 +57,17 @@ def process_precond_data(precond_parameters: str) -> dict:
 
     subspace = dict(name=subspace_name, parameters=parameters)
 
-    return dict(subspace=subspace, first_level_precond=first_level_precond)
+    # Retrieve firs-level preconditioner information
+    try:
+        first_precond, parameters = first_precond.split(':')
+
+    except ValueError:
+        first_precond = first_precond
+        parameters = None
+
+    first_precond = dict(name=first_precond, parameters=parameters)
+
+    return dict(subspace=subspace, first_precond=first_precond)
 
 
 def process_benchmark_data(benchmark_data: str) -> dict:
@@ -163,7 +173,7 @@ def compute_subspace_sizes(n_subspaces: int,
     return subspace_sizes
 
 
-def set_reference_run(operator: TestOperator, precond: str, n_runs: int = 100) -> ConjugateGradient:
+def set_reference_run(operator: TestOperator, precond: dict, n_runs: int = 50) -> dict:
     """
     Method to run the Conjugate Gradient a representative number of times, to select the right-hand 
     side corresponding to the average run.
@@ -174,31 +184,33 @@ def set_reference_run(operator: TestOperator, precond: str, n_runs: int = 100) -
     """
 
     # Initialize parameters
-    n, _ = operator.get('shape')
-    PATH = os.path.join(REFERENCES_RUN_ROOT_PATH, operator.get('name') + '.ref')
+    n, _ = operator['shape']
+    PATH = os.path.join(REFERENCES_RUN_ROOT_PATH, operator['name'] + '.ref')
+    precond_label = precond['name'] + '_' + str(precond['parameters'])
 
+    # Create file if not existing
     if not os.path.isfile(PATH):
         with open(PATH, 'wb') as file:
             pickle.Pickler(file).dump(dict())
 
+    # Open file corresponding to the operator under test
     with open(PATH, 'rb') as file:
         operators_run = pickle.Unpickler(file).load()
 
-        if precond not in operators_run.keys():
-            # Set a CG run of reference
-            rhs = list()
-            iterations = list()
-            lin_op = SelfAdjointMatrix(operator.get('operator'), def_pos=True)
-            M = AlgebraicPreconditionerFactory(lin_op).get(precond)
+        if precond_label not in operators_run.keys():
 
-            # Run the CG with several left-hand sides
+            # Set a CG run of reference
+            rhs, iterations = list(), list()
+            lin_op = SelfAdjointMatrix(operator['operator'], def_pos=True)
+            M = AlgebraicPreconditionerFactory(lin_op).get(precond['name'], precond['parameters'])
+
+            # Run the CG with randomly generated right-hand sides
             for i in range(n_runs):
                 # Create random linear system
                 lin_sys = LinearSystem(lin_op, lin_op.dot(numpy.random.randn(n, 1)))
 
                 # Run the Conjugate Gradient
                 cg = ConjugateGradient(lin_sys, x_0=None, M=M)
-                cg.run()
 
                 # Store results
                 rhs.append(lin_sys.rhs)
@@ -208,52 +220,47 @@ def set_reference_run(operator: TestOperator, precond: str, n_runs: int = 100) -
             n_iterations = numpy.asarray(iterations)
             average_run = int(numpy.argmin(numpy.abs(n_iterations - numpy.mean(n_iterations))))
 
-            # Set the average linear system
-            lin_sys = LinearSystem(lin_op, rhs[average_run])
-
-            # Run the Conjugate Gradient on this average linear system
-            reference_cg = ConjugateGradient(lin_sys, x_0=None, M=M, arnoldi=True, buffer=numpy.Inf)
-            reference_cg.run()
-
-            operators_run[precond] = reference_cg
+            operators_run[precond_label] = dict(n_iterations=n_iterations[average_run],
+                                                rhs=rhs[average_run])
 
             # Save results obtained
             with open(PATH, 'wb') as new_file:
                 pickle.Pickler(new_file).dump(operators_run)
 
-    return operators_run[precond]
+    return operators_run[precond_label]
 
 
-def initialize_report(operator: TestOperator, setup: dict, reference: ConjugateGradient) -> str:
+def initialize_report(operator: TestOperator, setup: dict, n_iterations: int) -> str:
     """
     Method to create a report file and write the header.
 
     :param operator: TestOperator containing the operator to benchmark on and its metadata.
     :param setup: dictionary containing the setup parameters.
-    :param reference: ConjugateGradient object with convergence data of the reference run.
+    :param n_iterations: number of iteration of the reference run with the first-level
+    preconditioner alone.
     """
     # Date and time fore report identification
     date_ = ''.join(time.strftime("%x").split('/'))
     time_ = ''.join(time.strftime("%X").split(':'))
 
     # Sampling method and its parameters
-    sampling = setup['subspace']['name'] + '#' + str(setup['subspace']['parameters'])
+    sampling = setup['subspace']['name'] + ':' + str(setup['subspace']['parameters'])
+    precond = setup['first_precond']['name'] + ':' + str(setup['first_precond']['parameters'])
 
     # Set the report name
-    report_name = '_'.join([operator.get('name'), date_, time_, sampling]) + '.rpt'
-
-    print(report_name)
+    report_name = '_'.join([operator['name'], date_, time_, precond, sampling]) + '.rpt'
 
     # Aggregate metadata of both the operator tested and the benchmark itself
-    operator_metadata = ', '.join([str(operator.get('rank')),
-                                  str(operator.get('non_zeros')),
-                                  str(operator.get('conditioning')),
-                                  operator.get('source')])
+    operator_metadata = ', '.join([str(operator['rank']),
+                                   str(operator['non_zeros']),
+                                   str(operator['conditioning']),
+                                   operator['source']])
 
-    benchmark_metadata = ', '.join([setup['first_level_precond'],
-                                   str(reference.output['n_iterations']),
-                                   str(setup['subspace']['name']),
-                                    str(setup['subspace']['parameters'])])
+    benchmark_metadata = ', '.join([setup['first_precond']['name'],
+                                    str(setup['first_precond']['parameters']),
+                                    setup['subspace']['name'],
+                                    str(setup['subspace']['parameters']),
+                                    str(n_iterations)])
 
     # Header writing
     with open('reports/' + report_name, 'w') as report_file:
@@ -262,8 +269,8 @@ def initialize_report(operator: TestOperator, setup: dict, reference: ConjugateG
         report_file.write('>  SOLVER ............. Conjugate Gradient \n')
         report_file.write('>  SUBSPACES TESTED ... ' + str(setup['n_subspaces']) + '\n')
         report_file.write('>  RUNS ............... ' + str(setup['n_tests']) + '\n')
-        report_file.write('>  PROBLEM NAME ....... ' + operator.get('name') + '\n')
-        report_file.write('>  REFERENCE RUN ...... ' + str(reference.output['n_iterations']) + '\n')
+        report_file.write('>  PROBLEM NAME ....... ' + operator['name'] + '\n')
+        report_file.write('>  REFERENCE RUN ...... ' + str(n_iterations) + '\n')
         report_file.write('> \n')
         report_file.write('>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<< \n')
         report_file.write('~operator_metadata: ' + operator_metadata + '\n')
@@ -273,49 +280,60 @@ def initialize_report(operator: TestOperator, setup: dict, reference: ConjugateG
     return report_name
 
 
-def benchmark(setup: dict,
-              subspaces: list,
-              reference: ConjugateGradient) -> None:
+def benchmark(operator: TestOperator, setup: dict, subspaces: list) -> None:
     """
     Process the benchmark of an operator with the specification contained in the setup
     dictionary. Compare the operator to a reference run and store the results in text file.
-    
+
+    :param operator: TestOperator containing the operator to benchmark on and its metadata.
     :param setup: Dictionary with benchmark parameters.
     :param subspaces: List of subspaces size to test.
-    :param reference: ConjugateGradient object meant to be the reference to compare with.
     """
 
     # Initialize variables
-    lin_sys = reference.lin_sys
-    lin_op = lin_sys.lin_op
-    first_level_precond = reference.M_i
+    lin_op = SelfAdjointMatrix(operator['operator'], def_pos=True)
+    lin_sys = LinearSystem(lin_op, operator['rhs'])
     n, _ = lin_op.shape
 
-    krylov_lin_sys = LinearSystem(lin_op, lin_op.dot(numpy.random.randn(n, 1)))
+    # Proceed to reference run
+    first_precond = AlgebraicPreconditionerFactory(lin_op).get(setup['first_precond']['name'],
+                                                               setup['first_precond']['parameters'])
+
+    ref_cg = ConjugateGradient(lin_sys, x_0=None, M=first_precond)
+    n_iterations = ref_cg.output['n_iterations']
+
+    # Initialize report text file
+    report_name = initialize_report(operator, setup, n_iterations)
 
     # Process the benchmark
     for k in tqdm.tqdm(subspaces):
         report_line = '{:4} | '.format(k)
 
         # Create the subspace factory producing subspaces of shape (n, k)
-        if setup['subspace']['name'] in KrylovSubspaceFactory.krylov_type.keys():
-            factory = KrylovSubspaceFactory(krylov_lin_sys, precond=first_level_precond)
+        if setup['subspace']['name'] in DeterministicSubspaceFactory.subspace_type.keys():
+            perturbation = 1e-2
+            mat = operator['operator'] + perturbation * scipy.sparse.eye(n)
+            mat_op = SelfAdjointMatrix(mat, def_pos=True)
+            rhs = operator['rhs'] + perturbation * (2 * numpy.random.rand(n, 1) - 1)
+            precond = AlgebraicPreconditionerFactory(mat_op).get(
+                setup['first_precond']['name'],
+                setup['first_precond']['parameters'])
+            factory = DeterministicSubspaceFactory(mat_op, precond=precond, rhs=rhs)
 
-        elif setup['subspace']['name'] in RandomSubspaceFactory.sampling_type.keys():
-            factory = RandomSubspaceFactory(n)
+        elif setup['subspace']['name'] in RandomSubspaceFactory.subspace_type.keys():
+            factory = RandomSubspaceFactory(lin_op)
         else:
             raise ValueError('Subspace type unrecognized.')
 
         # Process the required number of runs
         for i in range(setup['n_tests']):
             subspace = factory.get(setup['subspace']['name'], k, setup['subspace']['parameters'])
-            lmp = LimitedMemoryPreconditioner(lin_op, subspace, M=first_level_precond)
+            lmp = LimitedMemoryPreconditioner(lin_op, subspace, M=first_precond)
 
             cg = ConjugateGradient(lin_sys, x_0=None, M=lmp)
-            cg.run()
 
-            report_line += str(cg.output['n_iterations'] / reference.output['n_iterations']) + ','
+            report_line += str(cg.output['n_iterations'] / n_iterations) + ','
 
         # Write result in report text file
-        with open(os.path.join(REPORTS_ROOT_PATH, setup['report_name']), 'a') as report_file:
+        with open(os.path.join(REPORTS_ROOT_PATH, report_name), 'a') as report_file:
             report_file.write(report_line[:-1] + '\n')
