383694989a2b56ca42cc94000a52dd23457cbdfa
a-scotto <alex_scotto@live.fr>
2020-03-28 13:49:46 +0100

    Include changes in TestOperator and subspace factories structure.

--- benchmark.py
+++ benchmark.py
@@ -13,15 +13,14 @@ import numpy
 import argparse
 
 from utils.operator import load_operator
-from core.projection_subspace import KrylovSubspaceFactory, RandomSubspaceFactory
-from utils.benchmarking import read_setup, compute_subspace_sizes, initialize_report, \
-                               set_reference_run, benchmark
+from core.projection_subspace import DeterministicSubspaceFactory, RandomSubspaceFactory
+from utils.benchmarking import read_setup, compute_subspace_sizes, benchmark
 
 OPERATOR_ROOT_PATH = 'operators/'
 
 # Parse command line argument
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--setup', default='setup', help='Path to benchmark setup file')
+parser.add_argument('setup', default='setup', help='Path to benchmark setup file')
 args = parser.parse_args()
 
 
@@ -32,33 +31,27 @@ for setup in read_setup(args.setup):
         operator = load_operator(operator_path, display=True)
 
         # Scale the operator to enforce 1 lying in the spectrum of the operator
-        u = numpy.random.randn(operator.get('rank'), 1)
-        gamma = float(u.T.dot(u)) / float(u.T.dot(operator.get('operator').dot(u)))
-        operator.set('operator', gamma * operator.get('operator'))
+        u = numpy.random.randn(operator['rank'], 1)
+        gamma = float(u.T.dot(u)) / float(u.T.dot(operator['operator'].dot(u)))
+        operator['operator'] = gamma * operator['operator']
 
         # Compute subspace sizes
-        if setup['subspace']['name'] in KrylovSubspaceFactory.krylov_type.keys():
-            _type = KrylovSubspaceFactory.krylov_type[setup['subspace']['name']]
+        if setup['subspace']['name'] in DeterministicSubspaceFactory.subspace_type.keys():
+            _type = DeterministicSubspaceFactory.subspace_type[setup['subspace']['name']]
 
             subspaces = compute_subspace_sizes(setup['n_subspaces'],
-                                               operator.get('operator'),
+                                               operator['operator'],
                                                subspace_type=_type)
 
-        elif setup['subspace']['name'] in RandomSubspaceFactory.sampling_type.keys():
-            _type = RandomSubspaceFactory.sampling_type[setup['subspace']['name']]
+        elif setup['subspace']['name'] in RandomSubspaceFactory.subspace_type.keys():
+            _type = RandomSubspaceFactory.subspace_type[setup['subspace']['name']]
 
             subspaces = compute_subspace_sizes(setup['n_subspaces'],
-                                               operator.get('operator'),
+                                               operator['operator'],
                                                subspace_type='sparse')
 
         else:
             raise ValueError('Subspace type unrecognized.')
 
-        # Load or set reference run
-        reference = set_reference_run(operator, setup['first_level_precond'])
-
-        # Initialize report text file
-        setup['report_name'] = initialize_report(operator, setup, reference)
-
         # Process benchmark
-        benchmark(setup, subspaces, reference)
+        benchmark = (operator, setup, subspaces)
