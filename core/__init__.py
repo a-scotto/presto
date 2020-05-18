835528916bdefbc34bb171be96416bce6888d327
a-scotto <alex_scotto@live.fr>
2020-05-18 18:43:27 +0200

    Add docstring + __all__.

---
+++ core/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on May 15, 2020 at 16:07.
+
+@author: a.scotto
+
+Description:
+"""
+
+__all__ = ['linop', 'linsys', 'preconditioner', 'subspace']
