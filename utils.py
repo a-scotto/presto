161b4e9534b8328c9b6a155e34727de70cd1613c
a-scotto <alex_scotto@live.fr>
2020-11-02 15:18:46 +0100

    Add qr method, modify subspace_angle methode + Correct imports.

--- utils.py
+++ utils.py
@@ -14,13 +14,14 @@ import time
 import numpy
 import pickle
 import os.path
-import warnings
+import scipy.sparse
 import scipy.optimize
 
+from presto.algebra import *
 from typing import List, Tuple
-from core.algebra import LinearOperator
 
-__all__ = ['Timer', 'compute_subspace_dim', 'report_init', 'merge_reports', 'load_operator', 'random_surjection']
+__all__ = ['Timer', 'compute_subspace_dim', 'report_init', 'merge_reports', 'load_operator', 'random_surjection', 'qr',
+           'subspace_angles']
 
 
 class UtilsError(Exception):
@@ -199,107 +200,103 @@ def random_surjection(n: int, k: int) -> numpy.ndarray:
     numpy.random.shuffle(output)
     return output
 
-def angles(F, G, ip_B=None, compute_vectors=False, degree=False):
-    """Principal angles between two subspaces.
 
-    This algorithm is based on algorithm 6.2 in `Knyazev, Argentati. Principal
-    angles between subspaces in an A-based scalar product: algorithms and
-    perturbation estimates. 2002.` This algorithm can also handle small angles
-    (in contrast to the naive cosine-based svd algorithm).
+def qr(V, ip_A=None):
+    """
+    Process to the QR decomposition in the inner product provided. Namely for a (n, p) matrix V, the algorithm provides
+    two matrices Q and R such that V = QR with Q of shape (n, p) with its columns being conjugate with respect to the
+    inner product ip_A and R of shape (p, p) upper triangular.
+
+    :param V: Block columns to orthogonalize
+    :param ip_A: Symmetric positive-definite operator as the inner product
+    """
+
+    # Sanitize the arguments of the method
+    if not isinstance(V, (numpy.ndarray, scipy.sparse.spmatrix, LinearSubspace)):
+        raise UtilsError('Cannot process to the orthogonalization because of format problem.')
+
+    V = V.mat if isinstance(V, LinearSubspace) else V
+    V = V.todense() if scipy.sparse.isspmatrix(V) else V
+    ip_A = IdentityOperator(V.shape[0]) if ip_A is None else ip_A
+
+    # Process to the orthogonalization
+    A_ = V.T @ (ip_A @ V)
+    L_factor, _ = scipy.linalg.cho_factor(A_, lower=True, overwrite_a=False)
+
+    Q = scipy.linalg.solve_triangular(L_factor, V.T, lower=True).T
+    R = numpy.tril(L_factor).T
+
+    return Q, R
+
+
+def subspace_angles(F, G, ip_A=None, compute_vectors=False, degree=False):
+    """
+    Principal angles between two subspaces.
+
+    This algorithm is based on algorithm 6.2 in `Knyazev, Argentati. Principal angles between subspaces in an A-based
+    scalar product: algorithms and perturbation estimates. 2002.` This algorithm can also handle small angles (in
+    contrast to the naive cosine-based svd algorithm).
 
     :param F: array with ``shape==(N,k)``.
     :param G: array with ``shape==(N,l)``.
-    :param ip_B: (optional) angles are computed with respect to this
-      inner product. See :py:meth:`inner`.
-    :param compute_vectors: (optional) if set to ``False`` then only the angles
-      are returned (default). If set to ``True`` then also the principal
-      vectors are returned.
+    :param ip_A: (optional) angles are computed with respect to this inner product. See :py:meth:`inner`.
+    :param compute_vectors: (optional) if set to ``False`` then only the angles are returned (default). If set to
+    ``True`` then also the principal vectors are returned.
     :param degree: Whether to convert angles from radians to degrees.
-
-    :return:
-
-      * ``theta`` if ``compute_vectors==False``
-      * ``theta, U, V`` if ``compute_vectors==True``
-
-      where
-
-      * ``theta`` is the array with ``shape==(max(k,l),)`` containing the
-        principal angles
-        :math:`0\\leq\\theta_1\\leq\\ldots\\leq\\theta_{\\max\\{k,l\\}}\\leq
-        \\frac{\\pi}{2}`.
-      * ``U`` are the principal vectors from F with
-        :math:`\\langle U,U \\rangle=I_k`.
-      * ``V`` are the principal vectors from G with
-        :math:`\\langle V,V \\rangle=I_l`.
-
-    The principal angles and vectors fulfill the relation
-    :math:`\\langle U,V \\rangle = \
-    \\begin{bmatrix} \
-    \\cos(\\Theta) & 0_{m,l-m} \\\\ \
-    0_{k-m,m} & 0_{k-m,l-m} \
-    \\end{bmatrix}`
-    where :math:`m=\\min\\{k,l\\}` and
-    :math:`\\cos(\\Theta)=\\operatorname{diag}(\\cos(\\theta_1),\\ldots,\\cos(\\theta_m))`.
-    Furthermore,
-    :math:`\\theta_{m+1}=\\ldots=\\theta_{\\max\\{k,l\\}}=\\frac{\\pi}{2}`.
     """
-    # make sure that F.shape[1]>=G.shape[1]
+
+    # Make sure that F has more columns than G
     reverse = False
     if F.shape[1] < G.shape[1]:
         reverse = True
         F, G = G, F
 
-    QF, _ = qr(F, ip_B=ip_B)
-    QG, _ = qr(G, ip_B=ip_B)
+    _, p = G.shape
 
-    # one or both matrices empty? (enough to check G here)
-    if G.shape[1] == 0:
-        theta = numpy.ones(F.shape[1])*numpy.pi/2
-        U = QF
-        V = QG
+    # Set the inner product to work with
+    ip_A = IdentityOperator(F.shape[0]) if ip_A is None else ip_A
+
+    QF, _ = qr(F, ip_A=ip_A)
+    QG, _ = qr(G, ip_A=ip_A)
+
+    u, s, v = scipy.linalg.svd(QF.T @ (ip_A @ QG))
+    V_cos = QG @ v.T
+    n_large = numpy.flatnonzero((s**2) < 0.5).shape[0]
+    n_small = s.shape[0] - n_large
+    theta = numpy.r_[numpy.arccos(s[n_small:]), numpy.ones(F.shape[1] - p) * numpy.pi/2]
+
+    # Deal with small angles using sine measure
+    if n_small != 0:
+        RG = V_cos[:, :n_small]
+        S = RG - QF @ QF.T @ (ip_A @ RG)
+        _, R = qr(S, ip_A=ip_A)
+        u_, s_, v_ = scipy.linalg.svd(R)
+        theta = numpy.r_[numpy.arcsin(s_[::-1][:n_small]), theta]
     else:
-        Y, s, Z = scipy.linalg.svd(inner(QF, QG, ip_B=ip_B))
-        Vcos = numpy.dot(QG, Z.T.conj())
-        n_large = numpy.flatnonzero((s**2) < 0.5).shape[0]
-        n_small = s.shape[0] - n_large
-        theta = numpy.r_[
-            numpy.arccos(s[n_small:]),  # [-i:] does not work if i==0
-            numpy.ones(F.shape[1]-G.shape[1])*numpy.pi/2]
-        if compute_vectors:
-            Ucos = numpy.dot(QF, Y)
-            U = Ucos[:, n_small:]
-            V = Vcos[:, n_small:]
+        RG, v_ = None, None
 
-        if n_small > 0:
-            RG = Vcos[:, :n_small]
-            S = RG - numpy.dot(QF, inner(QF, RG, ip_B=ip_B))
-            _, R = qr(S, ip_B=ip_B)
-            Y, u, Z = scipy.linalg.svd(R)
-            theta = numpy.r_[
-                numpy.arcsin(u[::-1][:n_small]),
-                theta]
-            if compute_vectors:
-                RF = Ucos[:, :n_small]
-                Vsin = numpy.dot(RG, Z.T.conj())
-                # next line is hand-crafted since the line from the paper does
-                # not seem to work.
-                Usin = numpy.dot(RF, numpy.dot(
-                    numpy.diag(1/s[:n_small]),
-                    numpy.dot(Z.T.conj(), numpy.diag(s[:n_small]))))
-                U = numpy.c_[Usin, U]
-                V = numpy.c_[Vsin, V]
-
+    # Compute vectors related to principal angles
     if compute_vectors:
+        U_cos = numpy.dot(QF, u)
+        U = U_cos[:, n_small:]
+        V = V_cos[:, n_small:]
+
+        if RG is not None and v_ is not None:
+            RF = U_cos[:, :n_small]
+            V_sin = RG @ v_.T
+            U_sin = RF @ numpy.diag(1 / s[:n_small]) @ v_.T @ numpy.diag(s[:n_small])
+            U = numpy.c_[U_sin, U]
+            V = numpy.c_[V_sin, V]
+
         if reverse:
             U, V = V, U
 
         if not degree:
-            return theta[:G.shape[1]], U, V
+            return theta[:p], U, V
         else:
-            return numpy.rad2deg(theta[:G.shape[1]]), U, V
+            return numpy.rad2deg(theta[:p]), U, V
     else:
         if not degree:
-            return theta[:G.shape[1]]
+            return theta[:p]
         else:
-            return numpy.rad2deg(theta[:G.shape[1]])
-
+            return numpy.rad2deg(theta[:p])
